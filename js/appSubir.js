3d4b67edac46da36a5c81f6544f195a150c42b03
themike123 <themike123nova@gmail.com>
2016-01-19 23:38:14 -0600

    Primera version

--- js/appSubir.js
+++ js/appSubir.js
@@ -1,8 +1,51 @@
-var url_server = 'http://192.168.0.100:8080/';
-var app = angular.module('subir',[])
+function cargar() {
 
+    var ContenedorListas=localStorage.getItem("listas");
 
+    if (ContenedorListas) {
+      var aux=JSON.parse(ContenedorListas);
+      document.getElementById("contenedora").innerHTML = '';
+      for (var i = 0; i < aux.notas.length; i++) {
+        var cadena= "<div class=\"col s12 m6\">"+
+                    "<div class=\"card-panel teal\">"+
+                    "<span class=\"white-text\" >"+
+                    "<font  size=\"6\">"+aux.notas[i].contenido+
+                    "</span>"+
+                    "</div>"+
+                    "</div>";
 
-app.controller('subirController', function($scope, $http){
+        document.getElementById("contenedora").innerHTML += cadena;
+      }
+    }
 
+}
+
+function nuevo() {
+  var ContenedorListas=localStorage.getItem("listas");
+  console.log("Esto tiene localStorage:"+ContenedorListas);
+  var nota ={
+  title:'Hola Mundo',
+  contenido:document.getElementById("textarea1").value
+  }
+
+  console.log("contenido de mi nota:"+document.getElementById("textarea1").value);
+
+  if (ContenedorListas) {
+    var aux =JSON.parse(ContenedorListas);
+    aux.notas.push(nota);
+    localStorage.clear();
+    localStorage.removeItem("listas");
+    localStorage.setItem("listas",JSON.stringify(aux) );
+    console.log("Lo que tiene localStorage:"+localStorage.getItem("listas"));
+  }else {
+    var Contenedor={
+      'notas':[]
+    }
+    console.log("Esta es la nota:"+document.getElementById("textarea1").value);
+    Contenedor.notas.push(nota);
+    localStorage.setItem("listas",JSON.stringify(Contenedor) );
+    console.log("Lo que tiene localStorage:"+localStorage.getItem("listas"));
+  }
+
+  window.location="index.html";
 }
